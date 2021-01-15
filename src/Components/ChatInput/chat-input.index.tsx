3fdf751e5426437996f1a265dfc5406eafa55e19
Victor <victorpbi5@hotmail.com>
2021-01-15 18:29:28 -0300

    message input

---
+++ src/Components/ChatInput/chat-input.index.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import { TextInput, Button } from 'react-native-paper';
+
+interface ChatInputProps {
+    value: string;
+    onChangeText: (text: string) => void;
+    onSend: () => void;
+}
+
+const ChatInput: React.FC<ChatInputProps> = ({ value, onChangeText, onSend }) => {
+    return (
+        <View style={styles.container}>
+            <TextInput style={styles.input} value={value} onChangeText={onChangeText} accessibilityComponentType={undefined} accessibilityTraits={undefined} />
+            <Button onPress={onSend} icon="send" mode="text" style={styles.button} accessibilityComponentType={undefined} accessibilityTraits={undefined}>
+            </Button>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        width: "100%",
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    },
+    input: {
+        flex: 1,
+    },
+    button: {
+        backgroundColor: 'transparent'
+    }
+});
+
+export default ChatInput;
