5fc19d2ef5621f6700375e4fd335b02ecf79e202
Victor <victorpbi5@hotmail.com>
2021-01-15 20:11:36 -0300

    gifted chat

--- src/Screens/Chat/chat.index.tsx
+++ src/Screens/Chat/chat.index.tsx
@@ -1,26 +1,32 @@
-import React, { useState, useEffect } from 'react';
-import { StyleSheet, KeyboardAvoidingView, Platform } from 'react-native';
-import { FlatList } from 'react-native-gesture-handler';
+import React, { useState, useEffect, useRef } from 'react';
+import { StyleSheet, KeyboardAvoidingView, Platform, View, ScrollView, NativeScrollEvent, FlatList } from 'react-native';
+import { GiftedChat, IMessage } from 'react-native-gifted-chat';
 import ChatInput from '../../Components/ChatInput/chat-input.index';
 import MessageItem from '../../Components/MessageItem/message-item.index';
-import TopBar from '../../Components/TopBar/top-bar.index';
 
-import { Message } from '../../interfaces/interfaces';
 import { loremIpsum } from '../../util/utils';
 
 const Chat: React.FC = () => {
-    const [messages, setMessages] = useState<Array<Message>>([]);
+    const [messages, setMessages] = useState<Array<IMessage>>([]);
     const [text, setText] = useState("");
+    const listRef = useRef<ScrollView>(null);
 
     useEffect(() => {
-        const arr: Array<Message> = [];
+        const arr: Array<IMessage> = [];
 
         for (let i = 0; i < 50; i++) {
+            const sent = Math.random() > 0.5;
+
             arr.push({
-                id: i,
-                text: loremIpsum(3 + Math.floor(Math.random() * 5)),
-                createdAt: "13:25",
-                userId: Math.random() > 0.5 ? 1 : 0,
+                _id: i,
+                text: i + loremIpsum(3 + Math.floor(Math.random() * 5)),
+                createdAt: new Date(),
+                sent,
+                received: !sent,
+                user: {
+                    _id: sent ? 1 : 0,
+                    name: sent ? "Você" : "Papi",
+                }
             });
         }
 
@@ -29,31 +35,46 @@ const Chat: React.FC = () => {
 
     const sendMessage = () => {
         setMessages([{
-            id: messages.length,
-            createdAt: new Date().toISOString().substr(11, 5),
+            _id: messages.length,
+            createdAt: new Date(),
             text,
-            userId: 1,
+            user: {
+                _id: 1,
+                name: "Você",
+            },
         }, ...messages]);
 
         setText("");
 
-        setTimeout(() => setMessages([{
-            id: messages.length,
-            createdAt: new Date().toISOString().substr(11, 5),
+        listRef.current?.scrollToEnd();
+
+        setTimeout(() => setMessages(messages => [{
+            _id: messages.length,
+            createdAt: new Date(),
             text: loremIpsum(3 + Math.floor(Math.random() * 5)),
-            userId: 0,
+            user: {
+                _id: 0,
+                name: "Papi",
+            }
         }, ...messages]), 500);
     }
 
     const loadMore = () => {
-        const arr: Array<Message> = [];
+        const arr: Array<IMessage> = [];
 
         for (let i = 0; i < 10; i++) {
+            const sent = Math.random() > 0.5;
+
             arr.push({
-                id: i,
-                text: loremIpsum(3 + Math.floor(Math.random() * 5)),
-                createdAt: "13:25",
-                userId: Math.random() > 0.5 ? 1 : 0,
+                _id: messages.length + i,
+                text: i + loremIpsum(3 + Math.floor(Math.random() * 5)),
+                createdAt: new Date(),
+                sent,
+                received: !sent,
+                user: {
+                    _id: sent ? 1 : 0,
+                    name: sent ? "Você" : "Papi",
+                }
             });
         }
 
@@ -61,27 +82,69 @@ const Chat: React.FC = () => {
     }
 
     return (
-        <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding" : "height"}>
-            <TopBar />
-            <FlatList
-                inverted
-                data={messages}
-                renderItem={({ item }) => (
-                    <MessageItem message={item} />
-                )}
-                style={styles.list}
-                keyExtractor={({ id }) => id.toString()}
-                onEndReached={() => loadMore()}
-            />
+        <GiftedChat
+            messages={messages}
+            onSend={() => sendMessage()}
+            text={text}
+            onInputTextChanged={setText}
+            renderMessage={({ currentMessage }) => currentMessage && (
+                <MessageItem message={currentMessage} />
+            )}
+            onLoadEarlier={() => loadMore()}
+            loadEarlier={true}
+        />
 
-            <ChatInput value={text} onChangeText={setText} onSend={sendMessage} />
-        </KeyboardAvoidingView>
+        // <KeyboardAvoidingView style={styles.container} behavior={Platform.OS === "ios" ? "padding" : "height"}>
+        //     {/* <ScrollView
+        //                 ref={listRef}
+        //                 style={styles.list}
+        //                 onScroll={({ nativeEvent }) => {
+        //                     if (isCloseToTop(nativeEvent)) {
+        //                         loadMore()
+        //                     }
+        //                 }}
+        //             >
+        //                 {messages.map((message) => (
+        //                     <MessageItem key={message.id.toString()} message={message} />
+        //                 ))}
+        //             </ScrollView> */}
+
+        //     <View style={styles.listContainer}>
+        //         <FlatList
+        //             inverted
+        //             data={messages}
+        //             renderItem={({ item }) => (
+        //                 <MessageItem message={item} />
+        //             )}
+        //             style={styles.list}
+        //             keyExtractor={({ id }) => id.toString()}
+        //             onEndReached={() => loadMore()}
+        //         />
+        //     </View>
+
+        //     <View style={styles.inputContainer}>
+        //         <ChatInput value={text} onChangeText={setText} onSend={sendMessage} />
+        //     </View>
+        // </KeyboardAvoidingView>
     );
 }
 
 const styles = StyleSheet.create({
+    container: {
+        display: 'flex',
+        flexDirection: 'column',
+        flexGrow: 1,
+        justifyContent: 'space-around'
+    },
+    listContainer: {
+        flexGrow: 1
+    },
     list: {
-        width: "100%",
+        width: "100%"
+    },
+    inputContainer: {
+        flex: 1,
+        height: 100
     }
 });
 
